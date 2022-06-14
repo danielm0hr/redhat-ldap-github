d181d7390ff68e29180dd9d4cdbe6b73b04db22e
Daniel Mohr <dmohr@redhat.com>
2022-06-14 22:51:24 +0200

    Add RH LDAP information to ES

--- db/populate.py
+++ db/populate.py
@@ -6,6 +6,7 @@ import json
 import requests
 import os
 from datetime import datetime
+from io import BytesIO
 
 
 from elasticsearch import Elasticsearch
@@ -66,10 +67,10 @@ event_types = [
     'PullRequestEvent']
 
 # Start at this date
-start_year=2015
-start_month=9
-start_day=8
-start_hour=12
+start_year=2016
+start_month=1
+start_day=1
+start_hour=0
 
 for year in range(2015, 2023):
 
@@ -86,13 +87,13 @@ for year in range(2015, 2023):
                 if year<start_year:
                     continue;
                 else:
-                    if year==start_year and month<9:
+                    if year==start_year and month<start_month:
                             continue;
                     else:
-                        if month==9 and day<8:
+                        if month==start_month and day<start_day:
                             continue;
                         else:
-                            if day==8 and hour<12:
+                            if day==start_day and hour<start_hour:
                                 continue;
 
                 file_name = '{}-{:02d}-{:02d}-{}.json.gz'.format(year, month, day, hour)
@@ -103,44 +104,48 @@ for year in range(2015, 2023):
 
                 try:
                     print('Downloading '+dl_url)
-                    dl_file = requests.get(dl_url, stream=True,headers={'User-agent': 'Mozilla/5.0'})
+                    dl_file = requests.get(dl_url, timeout=10, stream=True,headers={'User-agent': 'Mozilla/5.0'})
 
                     if dl_file:
-                        open(file_name, 'wb').write(dl_file.content)
+                        with gzip.open(BytesIO(dl_file.content), 'rb') as archive:
+                            lines = archive.readlines()
+
+                            for line_json in lines:
+                                event = json.loads(line_json)
+
+                                if event['type'] in event_types:
+                                    if event['actor']['login'] in gh_users:
+                                        # print('Found matched Red Hat user '+event['actor']['login'])
+
+                                        event["redhat"] = dict()
+                                        event["redhat"]["uid"] = gh_users[event['actor']['login']]['uid']
+                                        event["redhat"]["rnd_comp"] = gh_users[event['actor']['login']]['rh_rnd_comp']
+                                        event["redhat"]["project"] = gh_users[event['actor']['login']]['rh_project']
+                                        event["redhat"]["manager"] = gh_users[event['actor']['login']]['manager_uid']
+
+                                        resp = es.index(index=es_index, document=event)
+                                        # print(resp['result'])
+
+                                        matched = matched+1
+                                        continue;
+                                    if event['type'] == 'PushEvent':
+                                        for commit in event['payload']['commits']:
+                                            if '@redhat.com' in commit['author']['email']:
+                                                # print('Found UNmatched Red Hat user '+event['actor']['login']+' '+commit['author']['email'])
+                                                resp = es.index(index=es_index, document=event)
+
+                                                unmatched = unmatched+1
+                                                break;
                     else:
                         print("Skipping {}, HTTP response {}".format(file_name, dl_file.status_code))
                         continue
 
-                    with gzip.open(file_name, 'rb') as archive:
-                        lines = archive.readlines()
-
-                        for line_json in lines:
-                            event = json.loads(line_json)
-                            if event['type'] in event_types:
-                                if event['actor']['login'] in gh_users:
-                                    # print('Found matched Red Hat user '+event['actor']['login'])
-
-                                    resp = es.index(index=es_index, document=event)
-                                    # print(resp['result'])
-
-                                    matched = matched+1
-                                    continue;
-                                if event['type'] == 'PushEvent':
-                                    for commit in event['payload']['commits']:
-                                        if '@redhat.com' in commit['author']['email']:
-                                            # print('Found UNmatched Red Hat user '+event['actor']['login']+' '+commit['author']['email'])
-                                            resp = es.index(index=es_index, document=event)
-
-                                            unmatched = unmatched+1
-                                            break;
-
-                    os.remove(file_name)
                 except Exception as e:
                     print(e)
                     print('Skipping {}'.format(file_name))
 
                     with open('download_errors', 'a') as errors:
-                        errors.write('{}: {} failed.'.format(datetime.now(), file_name))
+                        errors.write('{}: {} failed.\n'.format(datetime.now(), file_name))
 
 
             print('Found {} matched and {} unmatched records'.format(matched, unmatched))
